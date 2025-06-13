1a7186cad920d044b0943d8865a53eb01bc22a2e
TiagoSantoss30 <200319821+TiagoSantoss30@users.noreply.github.com>
2025-06-13 17:56:58 -0300

    feat(routes): add routes for Login and SignUp pages

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
 import About from "@pages/About";
+import SignUp from "@pages/SignUp";
+import Login from "@pages/Login";
+
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
