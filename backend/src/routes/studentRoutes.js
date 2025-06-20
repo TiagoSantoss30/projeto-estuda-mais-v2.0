2dfb799bf6ab60a0c56d5a338b541c6d28dbef00
TiagoSantoss30 <200319821+TiagoSantoss30@users.noreply.github.com>
2025-06-20 11:21:46 -0300

    chore(project): remove old backend and unused frontend files

--- backend/src/routes/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authMiddleware = require("../middleware/authMiddleware");
-const studentController = require("../controllers/studentController");
-
-router.get("/my-data", authMiddleware, studentController.getMyData);
-
-module.exports = router;
